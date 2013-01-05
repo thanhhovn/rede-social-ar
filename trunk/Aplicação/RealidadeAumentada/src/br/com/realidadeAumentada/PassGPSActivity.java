46ef2e38de9d22ae21e379a39065b295a957627a
tio.patinhas20@gmail.com <tio.patinhas20@gmail.com@b5ba1567-8c68-1651-3cc9-47d9cb1fe8f3>
2013-01-05 16:00:38 +0000

    Web Service funcionando.

--- trunk/Aplicação/RealidadeAumentada/src/br/com/realidadeAumentada/PassGPSActivity.java
+++ trunk/Aplicação/RealidadeAumentada/src/br/com/realidadeAumentada/PassGPSActivity.java
@@ -10,7 +10,7 @@ import br.com.realidadeAumentada.map.MyLocation;
 import com.google.android.maps.GeoPoint;
 
 
-public class UseGPSActivity extends Activity {
+public class PassGPSActivity extends Activity {
     /** Called when the activity is first created. */
 	private Handler mHandler = new Handler();
 	private TextView mLatitude;
@@ -18,7 +18,8 @@ public class UseGPSActivity extends Activity {
 	private MyLocation mMyLocation;
 	
 	
-    public void onCreate(Bundle savedInstanceState) {
+    @SuppressWarnings("static-access")
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gps);
     
@@ -26,18 +27,25 @@ public class UseGPSActivity extends Activity {
         mLongitude = (TextView) findViewById(R.id.longitude);
         
         mMyLocation = new MyLocation(this);
-        Runnable runnable = new Runnable() {
+        mMyLocation.turnGPSOnOff();
+        try {
+        	// Foi necessário adicionar uma Thread e colocá-la para dormir para atrasar o processo
+        	// Isso foi feito para corrigir o problema que ocorria durante a ativação do GPS
+        	// Antes se o GPS não estivesse abilitado a aplicação parava.
+			new Thread().sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    	Runnable runnable = new Runnable() {
         	public void run() {
-        		mHandler.post(new Runnable() {
+        		mHandler.postDelayed(new Runnable() {
 					public void run() {
 						GeoPoint geoPoint = mMyLocation.getMyLocation();
 						mLatitude.setText(geoPoint.getLatitudeE6()+"");
 						mLongitude.setText(geoPoint.getLongitudeE6()+"");
 					}
-				});
+				},1000);
         	}
-        	
-        	
         };
         mMyLocation.runOnFirstFix(runnable);
         mMyLocation.runOnLocationUpdate(runnable);
@@ -47,13 +55,25 @@ public class UseGPSActivity extends Activity {
 	
 	protected void onResume() {
 		mMyLocation.enableMyLocation();
+		mMyLocation.turnGPSOnOff();
 		super.onResume();
 	}
 	
 	
 	protected void onPause() {
 		mMyLocation.disableMyLocation();
+		mMyLocation.turnGPSOnOff();
 		super.onPause();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		try {
+			finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		super.onDestroy();
 	}
 
 	
--- trunk/Aplicação/RealidadeAumentada/src/br/com/realidadeAumentada/map/LocationManagerHelper.java
+++ trunk/Aplicação/RealidadeAumentada/src/br/com/realidadeAumentada/map/LocationManagerHelper.java
@@ -1,18 +1,31 @@
 package br.com.realidadeAumentada.map;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class LocationManagerHelper implements LocationListener {
 
+	
     private static double latitude;
     private static double longitude;
+    private static Calendar currentTimeStamp = null;
 
+    public LocationManagerHelper(Context context){
+    	LocationManager mlocManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,this);
+    }
     
     public void onLocationChanged(Location loc) {
         latitude = loc.getLatitude();
         longitude = loc.getLongitude();
+        
+        setCurrentTimeStamp(Calendar.getInstance());
     }
 
     
@@ -22,10 +35,7 @@ public class LocationManagerHelper implements LocationListener {
     public void onProviderEnabled(String provider) { }
 
     
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-
-    }
+    public void onStatusChanged(String provider, int status, Bundle extras) { }
 
     public static double getLatitude() {
         return latitude;
@@ -34,5 +44,17 @@ public class LocationManagerHelper implements LocationListener {
     public static double getLongitude() {
         return longitude;
     }
+
+	public static Date getCurrentTimeStamp() {
+		Date date = null;
+		if(currentTimeStamp != null){
+			date = currentTimeStamp.getTime(); 
+		}
+		return date;
+	}
+
+	public void setCurrentTimeStamp(Calendar currentTime) {
+		currentTimeStamp = currentTime;
+	}
 
 }
